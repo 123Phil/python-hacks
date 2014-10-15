4f4f91b2d59dd80b42d3fbb34bd55eea2a7bdc4c
123Phil <phillipwstewart@gmail.com>
2014-10-15 15:36:38 -0700

    Create renamer.py

---
+++ renamer.py
@@ -0,0 +1,7 @@
+# careful with this one...
+import os
+i = 1
+for fn in os.listdir('.'):
+  print fn, "Title S01" + "{0:02d}".format(i) + fn[-4:]
+#	os.rename(fn, "Title S01" + "{0:02d}".format(i) + fn[-4:])
+	i += 1
