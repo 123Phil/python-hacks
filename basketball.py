eef0e62bee7122f2a7029d9219d8a668ff816032
123Phil <phillipwstewart@gmail.com>
2014-10-15 15:28:25 -0700

    Create basketball.py

---
+++ basketball.py
@@ -0,0 +1,463 @@
+# Phillip Stewart
+# I think this was for the facebook hacker cup...
+
+# sample input:
+'''
+20
+7 93 2
+Paul 82 189
+Kittipat 62 126
+Thomas 17 228
+Fabien 57 233
+Yifei 65 138
+Liang 92 100
+Victor 53 124
+11 116 1
+Daniel 89 224
+Andriy 78 181
+Chirag 91 206
+Fabien 45 162
+Wesley 31 166
+Vasily 57 203
+Wesley 88 174
+Song 91 234
+Purav 5 230
+Andrei 88 178
+Jordan 56 104
+25 103 2
+Sharad 11 217
+Aravind 22 127
+Thomas 91 212
+John 68 199
+Josh 92 160
+Doan 43 205
+Weiyan 1 125
+Dhruv 47 227
+John 15 154
+Tom 25 141
+Viswanath 87 177
+Rajat 42 150
+Lovro 83 116
+Rudradev 19 167
+Mehdi 90 213
+Paul 10 235
+Xiao 27 100
+Sharad 89 226
+Steaphan 69 228
+David 17 111
+Steaphan 16 200
+Liang 30 206
+Victor 61 125
+Sanjeet 74 216
+Weiyan 64 185
+19 86 5
+Andras 13 108
+David 90 125
+Vladislav 11 103
+Erling 90 119
+Doan 20 207
+Torbjorn 8 147
+Yifei 26 210
+Sanjeet 81 126
+Meihong 38 108
+Chi 60 173
+Lingjuan 15 175
+Yingsheng 92 143
+Dhruv 80 168
+Yingsheng 20 105
+Anh 11 187
+Saransh 38 199
+Vladislav 3 197
+Wesley 64 197
+Wai 62 181
+24 80 3
+Amol 52 169
+David 35 118
+Dmytro 37 222
+Manohar 40 179
+Roman 83 107
+Tom 43 238
+John 31 220
+Tom 60 169
+Paul 46 187
+Steaphan 81 123
+Andriy 26 103
+Chad 99 101
+Jan 31 129
+Torbjorn 84 207
+Slawek 28 159
+Nathan 40 140
+Daniel 61 129
+Yintao 52 162
+Ekansh 14 223
+Sharad 53 232
+Josh 51 174
+Anil 86 206
+Atol 0 218
+Torbjorn 40 141
+2 98 1
+Lingjuan 65 193
+Zejia 9 213
+4 72 1
+Anh 2 187
+Erling 69 226
+Purav 0 199
+Zejia 29 163
+18 95 2
+Liang 53 146
+Wesley 12 197
+Wenjie 86 238
+Manohar 94 129
+Anshuman 27 180
+Chi 73 162
+David 16 238
+Purav 57 227
+Wai 42 196
+Chad 48 239
+Zainab 46 175
+Wai 98 114
+Chi 45 209
+Paul 17 135
+Paul 10 171
+Atol 23 162
+Yifei 99 197
+Wei 0 164
+10 57 3
+Fabien 0 111
+Duc 49 198
+Ekansh 98 121
+Zef 30 122
+Dmytro 50 124
+Chirag 39 209
+Doan 20 229
+Clifton 50 129
+Andrei 91 177
+Erling 0 118
+28 20 2
+Yintao 12 206
+Anh 11 178
+Aleksandar 2 140
+Jan 51 234
+Kittipat 79 151
+Zainab 71 169
+Sanjeet 96 206
+Wesley 74 201
+Anh 27 184
+Ahmed 73 171
+Andrei 56 114
+Dhruv 37 106
+Ranjeeth 45 149
+Lingjuan 77 113
+Clifton 39 229
+Ahmed 44 121
+Ahmed 97 188
+Lin 8 202
+Slawek 39 174
+Lingjuan 84 202
+Zainab 30 130
+Viswanath 70 107
+Dmytro 1 115
+Luiz 72 221
+Steaphan 31 157
+Gaurav 36 225
+Liang 85 224
+Anshuman 54 179
+8 59 3
+Anil 38 180
+Song 7 187
+David 65 159
+Lin 45 121
+Ranjeeth 39 183
+Torbjorn 26 181
+Clifton 57 158
+Phil 3 183
+16 21 5
+Andrei 35 112
+Andrei 66 130
+Nima 42 117
+Voja 96 150
+Aleksandar 72 117
+John 49 225
+Roman 50 215
+Aravind 28 143
+Doan 65 136
+Andrii 10 219
+Vladislav 49 166
+Keegan 66 140
+Paul 29 158
+Rudradev 2 174
+David 47 197
+Amol 4 104
+4 115 2
+Manohar 16 215
+Meihong 87 208
+Anshuman 95 154
+Nima 92 132
+23 82 3
+Lingjuan 50 198
+Zhen 74 108
+Ranjeeth 48 192
+Wesley 64 176
+Saransh 22 204
+Mehdi 68 117
+Wei 25 102
+Jan 97 150
+Torbjorn 12 198
+Jan 41 170
+Xiao 12 237
+Xiao 27 171
+Gaurav 87 237
+Andriy 90 178
+David 72 235
+Oleksandr 30 190
+Ekansh 7 155
+Zihing 80 196
+Wenjie 7 166
+Zhen 64 214
+Yingsheng 0 139
+Andriy 79 166
+Ajay 78 152
+25 21 2
+Keegan 80 196
+Lin 47 184
+Purav 21 142
+Dhruv 8 198
+Nima 9 233
+John 57 141
+Chi 40 176
+Aravind 31 101
+Oleksandr 97 240
+Kittipat 91 208
+Igor 28 178
+Keegan 62 149
+Vasily 89 103
+Victor 59 107
+Lin 57 140
+Gaurav 3 195
+Viswanath 69 178
+Yintao 85 228
+Saransh 97 129
+Rajat 86 193
+Jan 13 198
+Anh 71 183
+Anil 18 137
+Sanjeet 12 180
+Mehdi 28 129
+19 109 3
+Atol 21 171
+Jan 56 128
+Yintao 17 176
+Ekansh 44 194
+Jan 40 216
+Ahmed 26 219
+Victor 82 131
+Amol 33 174
+Doan 16 175
+Zejia 9 214
+Viswanath 68 139
+Kittipat 93 124
+Ajay 35 103
+Anil 1 185
+Paul 6 217
+Zihing 34 196
+Aravind 60 183
+Rudradev 48 163
+Wenjie 19 229
+28 15 4
+Oleksandr 34 111
+Mehdi 62 194
+Rudradev 52 123
+Chad 9 215
+Wenjie 49 197
+Doan 86 132
+Saransh 68 107
+Clifton 34 212
+Keegan 48 181
+Ekansh 17 162
+Purav 96 170
+Atol 50 225
+Dhruv 39 190
+Zhen 47 108
+Lovro 49 200
+Nathan 39 168
+Liang 39 224
+Clifton 83 232
+Paul 3 131
+John 79 121
+Ahmed 11 121
+Viswanath 31 177
+Dhruv 20 235
+Song 53 184
+Atol 92 195
+John 72 123
+Clifton 55 115
+Rajat 13 214
+25 50 5
+Aleksandar 34 200
+Zihing 41 124
+Andrii 14 226
+Voja 91 228
+Lin 83 182
+Erling 53 150
+Liang 74 233
+Luiz 49 125
+Atol 9 182
+Yingsheng 82 170
+Nima 71 228
+Rudradev 99 142
+Song 24 221
+Chad 39 195
+Bhuwan 54 146
+Bhuwan 12 233
+Zainab 88 180
+Voja 79 135
+Purav 79 203
+Fabien 90 230
+Nathan 12 148
+Ranjeeth 75 133
+Vladislav 88 148
+Viswanath 61 158
+Luiz 13 163
+10 78 2
+Xiao 51 110
+Philip 18 141
+Zejia 51 227
+Anshuman 100 183
+Tom 12 193
+Erling 36 190
+Philip 0 160
+Zhen 4 203
+Atol 57 106
+Wesley 15 101
+6 3 2
+Wai 99 131
+Weiyan 81 155
+Lin 80 100
+Purav 86 198
+Slawek 80 192
+Meihong 44 109
+'''
+
+import sys
+import operator
+
+def player_out(court):
+	minutes = 0
+	rank = 0
+	i = 0
+	p = 0
+	for player in court:
+		if player[2] > minutes:
+			minutes = player[2]
+			rank = player[1]
+			p = i
+		elif player[2] == minutes:
+			if player[1] > rank:
+				minutes = player[2]
+				rank = player[1]
+				p = i
+		i += 1
+	return p
+
+def player_in(bench):
+	minutes = bench[0][2]
+	rank = 999
+	i = 0
+	p = 0
+	for player in bench:
+		if player[2] < minutes:
+			minutes = player[2]
+			rank = player[1]
+			p = i
+		elif player[2] == minutes:
+			if player[1] < rank:
+				minutes = player[2]
+				rank = player[1]
+				p = i
+		i += 1
+	return p
+		
+
+def get_playerstring(t1, t2, M, P):
+	players = []
+	if len(t1) == P:
+		for i in xrange(P):
+			players.append(t1[i])
+	else:
+		bench = []
+		i = 0
+		for player in t1:
+			bench.append([player, i, 0])
+			i += 1
+			
+		court = []
+		for i in xrange(P):
+			court.append(bench.pop(0))
+		
+		for minute in xrange(M):
+			for i in xrange(P):
+				court[i][2] += 1
+			p_out = court.pop(player_out(court))
+			court.append(bench.pop(player_in(bench)))
+			bench.append(p_out)
+		
+		for i in xrange(P):
+			players.append(court[i][0])
+		
+	if len(t2) == P:
+		for i in xrange(P):
+			players.append(t2[i])
+	else:
+		bench = []
+		i = 0
+		for player in t2:
+			bench.append([player, i, 0])
+			i += 1
+			
+		court = []
+		for i in xrange(P):
+			court.append(bench.pop(0))
+		
+		for minute in xrange(M):
+			for i in xrange(P):
+				court[i][2] += 1
+			p_out = court.pop(player_out(court))
+			court.append(bench.pop(player_in(bench)))
+			bench.append(p_out)
+		
+		for i in xrange(P):
+			players.append(court[i][0])
+	
+	players.sort()
+	return ' '.join(players)
+	
+
+def main(args):
+	if len(args) != 2:
+		print 'Usage ~$ ' + args[0] + ' filename.txt'
+		sys.exit(0)
+	else:
+		filename = args[1]
+	
+	lines = []
+	with open(filename, 'r') as f:
+		for line in f:
+			lines.append(line.strip().split())
+	
+	numCases = int(lines[0][0])
+	lineCtr = 1
+	for i in xrange(numCases):
+		N, M, P = [int(x) for x in lines[lineCtr]]
+		lineCtr += 1
+		players = lines[lineCtr:lineCtr + N]
+		players.sort(key = lambda x: (int(x[1]), int(x[2])), reverse = True)
+		team1 = [p[0] for p in players[::2]]
+		team2 = [p[0] for p in players[1::2]]
+		
+		print 'Case #' + str(i+1) + ': ' + get_playerstring(team1, team2, M, P)
+		#print team1
+		#print team2
+		lineCtr += N
+
+if __name__ == "__main__":
+	main(sys.argv)
